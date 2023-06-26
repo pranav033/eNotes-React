8876b970c2fbbfb6fa685535659fa863b082ca0c
pranav033 <44546871+pranav033@users.noreply.github.com>
2023-06-26 21:32:59 +0530

    delete

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017";
-const connectToMongo = ()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected to mongo");
-    })
-}
-
-module.exports = connectToMongo
